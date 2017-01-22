51227ae48a0664ee7c21a59e9cbd933834ac4a73
James Morris <aerods7@gmail.com>
2017-01-22 18:42:34 +0000

    Badge ordering update

--- server/data/badge.js
+++ server/data/badge.js
@@ -8,7 +8,7 @@ exports.get = function(data, done) {
     if (data.section) { where += ' and (section = ? or section is null)'; values.push(data.section); }
     if (data.purpose == 'progress') { where += ' and type != ?'; values.push('core'); }
 
-    var query = 'SELECT * FROM badges ' + where + ' ORDER BY ordering';
+    var query = 'SELECT * FROM badges ' + where + ' ORDER BY ordering, name';
     db.get().query(query, values, function (err, rows) {
         if (err) return done(err)
         done(null, rows)
